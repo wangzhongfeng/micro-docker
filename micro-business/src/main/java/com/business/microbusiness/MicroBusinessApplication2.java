dab134840229041a3f8e9fb9576a439cbbda315e
wangzhongfeng <wangzhongfeng.xa@mopon.cn>
2018-09-04 11:18:50 +0800

    mybatis配置

--- micro-business/src/main/java/com/business/microbusiness/MicroBusinessApplication2.java
+++ micro-business/src/main/java/com/business/microbusiness/MicroBusinessApplication2.java
@@ -1,12 +1,10 @@
 package com.business.microbusiness;
 
-//import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-//import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableEurekaClient
